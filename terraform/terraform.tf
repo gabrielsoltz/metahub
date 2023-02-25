21037b903cdc652f2b4257f88039243ea4830157
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-02-25 12:35:30 -0300

    lambda_terraform

---
+++ terraform/terraform.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id          = data.aws_caller_identity.current.account_id
+  prefix              = "metahub"
+}
