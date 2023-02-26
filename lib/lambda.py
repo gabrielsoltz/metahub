64cee8e022522abdedbab1a35a7bab747f60056b
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-02-26 14:55:38 -0300

    lambda

--- lib/lambda.py
+++ lib/lambda.py
@@ -3,9 +3,16 @@ from os import environ
 import lib.main
 
 def lambda_handler(event, context):
-    # print (event)
-    # print (context)
-    # for key in environ:
-    #     print(key, '=>', environ[key])
-    exec = lib.main.main(["--output-modes", "lambda", "--no-banners"])
+
+    # This could be improved:
+    # - Reading from the event that could be a finding
+    # - Dynamic options from environment or event
+
+    LAMBDA_OPTIONS = ["--output-modes", "lambda", "--no-banners"]
+    CUSTOM_OPTIONS = []
+    # CUSTOM_OPTIONS = ["--sh-filters", "Id=010101010101"]
+
+    OPTIONS = LAMBDA_OPTIONS + CUSTOM_OPTIONS
+
+    exec = lib.main.main(OPTIONS)
     return exec
