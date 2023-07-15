b389115a34edd5d26a74f61b87f046a70279a4f3
Gabriel Alejandro Soltz <thegaby@gmail.com>
2023-07-16 00:02:09 +0200

    Fix: trusted_accounts was not empty

--- lib/config/configuration.py
+++ lib/config/configuration.py
@@ -1,3 +1,9 @@
-trusted_accounts = [""]
+# Security Hub Configurations
 
 sh_default_filters = {"RecordState": ["ACTIVE"], "WorkflowStatus": ["NEW"]}
+
+
+# MetaCheck configurations
+
+# List of AWS accounts that are trusted and not considered as external. This is used in the is_principal_external MetaCheck for policies.
+trusted_accounts = []
